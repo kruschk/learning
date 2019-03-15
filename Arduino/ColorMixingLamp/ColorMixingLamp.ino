489361e554252012ff6ef9bedda2ae4357dfdfd2
Jonah Kruschke <jonah.kruschke@gmail.com>
2019-03-14 21:36:39 -0600

    Add files via upload

---
+++ Arduino/ColorMixingLamp/ColorMixingLamp.ino
@@ -0,0 +1,55 @@
+const int greenLEDPin = 9;
+const int blueLEDPin = 10;
+const int redLEDPin = 11;
+
+const int greenSensorPin = A0;
+const int blueSensorPin = A1;
+const int redSensorPin = A2;
+
+int greenValue = 0;
+int blueValue = 0;
+int redValue = 0;
+
+int greenSensorValue = 0;
+int blueSensorValue = 0;
+int redSensorValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(greenLEDPin,OUTPUT);
+  pinMode(blueLEDPin,OUTPUT);
+  pinMode(redLEDPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  greenSensorValue = analogRead(greenSensorPin);
+  delay(5);
+  blueSensorValue = analogRead(blueSensorPin);
+  delay(5);
+  redSensorValue = analogRead(redSensorPin);
+
+  Serial.print("Raw Sensor Values: \t Red: ");
+  Serial.print(redSensorValue);
+  Serial.print("\t Green: ");
+  Serial.print(greenSensorValue);
+  Serial.print("\t Blue: ");
+  Serial.println(blueSensorValue);
+
+  redValue = redSensorValue/4;
+  greenValue = greenSensorValue/4;
+  blueValue = blueSensorValue/4;
+
+  Serial.print("Mapped Sensor Values: \t Red:");
+  Serial.print(redValue);
+  Serial.print("\t Green: ");
+  Serial.print(greenValue);
+  Serial.print("\t Blue: ");
+  Serial.println(blueValue);
+
+  analogWrite(redLEDPin,redValue);
+  analogWrite(greenLEDPin,greenValue);
+  analogWrite(blueLEDPin,blueValue);
+}
---
+++ Arduino/DigitalHourglass/DigitalHourglass.ino
@@ -0,0 +1,35 @@
+const int switchPin = 8;
+unsigned long previousTime = 0;
+int switchState = 0;
+int prevSwitchState = 0;
+int led = 2;
+long interval = 2000;
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int x = 2; x < 8; x++) {
+    pinMode(x, OUTPUT);
+  }
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long currentTime = millis();
+  if (currentTime - previousTime > interval) {
+    previousTime = currentTime;
+    digitalWrite(led, HIGH);
+    led++;
+    if (led == 7) {
+    }
+  }
+  switchState = digitalRead(switchPin);
+  if (switchState != prevSwitchState) {
+    for (int x = 2; x < 8; x++) {
+      digitalWrite(x, LOW);
+    }
+    led = 2;
+    previousTime = currentTime;
+  }
+  prevSwitchState = switchState;
+}
