7f0246430b66444d52cffac1093ec009b241078e
Jonah Kruschke <jonah.kruschke@gmail.com>
2019-12-19 21:22:09 -0500

    Add some MDN server-side programming tutorials
    The tutorials both build a library website, including a database for
    books, using django and express, respectively. See
    https://developer.mozilla.org/en-US/docs/Learn for more information.

---
+++ tutorials/web/mdn/server-side_programming/django/django_projects/locallibrary/catalog/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_books_with_the = Book.objects.filter(title__icontains='the').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+    
+    context = {
+        'num_books': num_books,
+        'num_books_with_the': num_books_with_the,
+        'num_genres': num_genres,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
---
+++ tutorials/web/mdn/server-side_programming/express/myapp/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.listen(8000, () => {
+    console.log("Example app listening on port 8000!");
+});
